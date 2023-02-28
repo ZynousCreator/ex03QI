c45ae538b3cf12d80d2109bfc93d597da0ebce93
Augustus Souza dos Santos <augustus.mp2@gmail.com>
2023-02-28 16:52:09 -0300

    feat: atualização dos exercicios 3 e 4
    correção do arquivo:
    	modified:   ex03.dart
    
    novas opções de interação de dados:
    	modified:   ex03.dart
    	modified:   ex04.dart

--- ex04.dart
+++ ex04.dart
@@ -1,13 +1,30 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
+  print('Select option');
+  print('1 Random');
+  print('2 Manual');
+  int option = int.parse(stdin.readLineSync()!);
   List<int> A = [];
-  for (int i = 0; i < 5; i++) {
-    print('Insert number');
-    int number = int.parse(stdin.readLineSync()!);
-    A.add(number);
+  switch (option) {
+    case 1:
+      List<int> B = List.generate(100, (_) => Random().nextInt(50));
+      for (int i = 0; i < B.length; i++) {
+        A.add(B[i]);
+      }
+      // print(B);
+      break;
+    case 2:
+      for (int i = 0; i < 100; i++) {
+        print('Insert number');
+        int number = int.parse(stdin.readLineSync()!);
+        A.add(number);
+      }
+      break;
   }
-  for (int i = 0; i < 5; i++) {
+//  print(A);
+  for (int i = 0; i < A.length; i++) {
     if (A[i] < 11) {
       print('N[$i] = ${A[i]}');
     }
